65098bc9a165d7407d8a91875d8c4307dff81642
Soribel S.B <soribelsantos0514@gmail.com>
2024-12-26 22:09:04 -0400

    Create getUrlsFromHTML and tests

--- crawler.js
+++ crawler.js
@@ -1,3 +1,32 @@
+const {JSDOM} = require('jsdom');
+
+function getUrlsFromHTML(HTMLBody, baseURL){
+    const urls = [];
+    const dom = new JSDOM(HTMLBody);
+    const linkElements = dom.window.document.querySelectorAll('a');
+    for(const linkElement of linkElements){
+        if (linkElement.href.slice(0,1) === "/") {
+            //relative
+            try{
+                const urlObjec = new URL(`${baseURL}${linkElement.href}`);
+                urls.push(urlObjec.href);
+            }catch(err){
+                console.log("error invalid url: ",err.message); 
+            }
+
+        } else {
+            //absolute
+            try{
+                const urlObjec = new URL(`${linkElement.href}`);
+                urls.push(urlObjec.href);
+            }catch(err){
+                console.log("error invalid url: ",err.message); 
+            }
+        }
+    }
+    return urls;
+}
+
 function normalizeURL(URLstring){
     let urlObjec = new URL(URLstring);
     const hostName = `${urlObjec.hostname}${urlObjec.pathname}`;
@@ -6,4 +35,7 @@ function normalizeURL(URLstring){
     }
     return hostName;
 }
-module.exports = {normalizeURL};
+module.exports = {
+    normalizeURL,
+    getUrlsFromHTML
+}
--- crawler.test.js
+++ crawler.test.js
@@ -1,4 +1,4 @@
-const {normalizeURL} = require('./crawler.js');
+const {normalizeURL,getUrlsFromHTML} = require('./crawler.js');
 const {test,expect} = require('@jest/globals');
 
 test('normalizeURL erase protocol',() => {
@@ -29,6 +29,77 @@ test('normalizeURL erase protocol',() => {
     const input = "http://fullstackopen.com";
     const actual = normalizeURL(input);
     const expected = "fullstackopen.com";
+
+    expect(actual).toEqual(expected);
+})
+
+test('getUrlsFromHTML',() => {
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href = "http://fullstackopen.com/">
+                FulstackOpen
+            </a>
+        </body>
+    </html>
+    `;
+    const inputBasedUrl = "";
+    const actual = getUrlsFromHTML(inputHTMLBody,inputBasedUrl);
+    const expected = ["http://fullstackopen.com/"];
+
+    expect(actual).toEqual(expected);
+})
+
+test('getUrlsFromHTML relative',() => {
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href = "/en/about">
+                FulstackOpen
+            </a>
+        </body>
+    </html>
+    `;
+    const inputBasedUrl = "https://fullstackopen.com";
+    const actual = getUrlsFromHTML(inputHTMLBody,inputBasedUrl);
+    const expected = ["https://fullstackopen.com/en/about"];
+
+    expect(actual).toEqual(expected);
+})
+
+test('getUrlsFromHTML many',() => {
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href = "/en/about">
+                FulstackOpen one
+            </a>
+            <a href = "/en/#course-contents">
+                FulstackOpen two
+            </a>
+        </body>
+    </html>
+    `;
+    const inputBasedUrl = "https://fullstackopen.com";
+    const actual = getUrlsFromHTML(inputHTMLBody,inputBasedUrl);
+    const expected = ["https://fullstackopen.com/en/about","https://fullstackopen.com/en/#course-contents"];
+
+    expect(actual).toEqual(expected);
+})
+
+test('getUrlsFromHTML invalid',() => {
+    const inputHTMLBody = `
+    <html>
+        <body>
+            <a href = "invalid">
+                FulstackOpen two
+            </a>
+        </body>
+    </html>
+    `;
+    const inputBasedUrl = "https://fullstackopen.com";
+    const actual = getUrlsFromHTML(inputHTMLBody,inputBasedUrl);
+    const expected = [];
 
     expect(actual).toEqual(expected);
 })
