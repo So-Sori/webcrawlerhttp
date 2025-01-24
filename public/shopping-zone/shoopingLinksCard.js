2c7ae3a2cc83138ecd893a6ab75c97e0672d2c28
Soribel S.B <soribelsantos0514@gmail.com>
2025-01-24 13:53:08 -0400

    Update imgs size and cards html structure

--- public/shopping-zone/shoopingLinksCard.js
+++ public/shopping-zone/shoopingLinksCard.js
@@ -1,27 +1,31 @@
-export async function shoopingLinksCard(currentPageUrl) {  
-  let cardProducs = document.createElement("div");
-  cardProducs.className = "card-product";
-  let resp = await fetch(`http://localhost:3000/proxy/${currentPageUrl}`);
+export async function shoopingLinksCard(arrPages, containerProducsCard) {
+  for (const linkPage of arrPages) {
+    if (!(arrPages.indexOf(linkPage) === 0)) {
+      let resp = await fetch(`http://localhost:3000/proxy/${linkPage}`);
 
-  if (resp.status > 399) {
-    return `Error in fetch status code: ${resp.status}, on page ${currentPageUrl}`;
-  }
-  const contentType = resp.headers.get("content-type");
-  if (!contentType.includes("text/html")) {
-    return `non html response: ${contentType}, on page ${currentPageUrl}`;
-  }
-  
-  let HTMLBody = await resp.text();
-  const dom = new DOMParser().parseFromString(HTMLBody, "text/html");
-  const img = dom.querySelector("#main-image");
-  const title = dom.getElementById("title");
-  const dolarSing = dom.querySelector(".a-price-symbol");
-  const price = dom.querySelector(".a-price-whole");
-    
-  cardProducs.innerHTML = `
+      if (resp.status > 399) {
+        return `Error in fetch status code: ${resp.status}, on page ${linkPage}`;
+      }
+      const contentType = resp.headers.get("content-type");
+      if (!contentType.includes("text/html")) {
+        return `non html response: ${contentType}, on page ${linkPage}`;
+      }
+
+      let HTMLBody = await resp.text();
+      const dom = new DOMParser().parseFromString(HTMLBody, "text/html");
+      const img = dom.querySelector("#main-image");
+      const title = dom.getElementById("title");
+      const dolarSing = dom.querySelector(".a-price-symbol");
+      const price = dom.querySelector(".a-price-whole");
+
+      containerProducsCard.innerHTML += `
+      <div class="card-product">
         ${img.outerHTML}
         ${title.innerHTML}
         ${dolarSing.innerHTML}${price.innerHTML}
-    `;
-  return cardProducs;
+      </div>
+        `;
+    }
+  }
+  return containerProducsCard;
 }
