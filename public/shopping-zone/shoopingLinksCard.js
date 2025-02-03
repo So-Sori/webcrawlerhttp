f889639ff4aeb5ceb092ef68315ea41451df3bea
Soribel S.B <soribelsantos0514@gmail.com>
2025-02-03 16:01:33 -0400

    Fix bug in savingPercent bc was null

--- public/shopping-zone/shoopingLinksCard.js
+++ public/shopping-zone/shoopingLinksCard.js
@@ -1,50 +1,60 @@
 export async function shoopingLinksCard(arrPages, containerProducsCard) {
+  // console.log(containerProducsCard);
+  // console.log(containerProducsCard.innerHTML);
+  
   for (const linkPage of arrPages) {
     if (!(arrPages.indexOf(linkPage) === 0)) {
-      let resp = await fetch(`http://localhost:3000/proxy/${linkPage}`);
+      try {
+        let resp = await fetch(`http://localhost:3000/proxy/${linkPage}`);
 
-      if (resp.status > 399) {
-        return `Error in fetch status code: ${resp.status}, on page ${linkPage}`;
-      }
-      const contentType = resp.headers.get("content-type");
-      if (!contentType.includes("text/html")) {
-        return `non html response: ${contentType}, on page ${linkPage}`;
-      }
+        if (resp.status > 399) {
+          throw new Error(`Error in fetch status code: ${resp.status}`);
+        }
+        const contentType = resp.headers.get("content-type");
+        if (!contentType.includes("text/html")) {
+          throw new Error(`non html response: ${contentType}, on page ${linkPage}`);
+        }
 
-      let HTMLBody = await resp.text();
-      const dom = new DOMParser().parseFromString(HTMLBody, "text/html");
-      const img = dom.querySelector("#main-image");
-      const title = dom.getElementById("title");
-      const dolarSing = dom.querySelector(".a-price-symbol");
-      const price = dom.querySelector(".a-price-whole");
-      const savingPercent = dom.querySelector("._cDEzb_apex-savings-percent_nsC2Z");
-      const realPrice = dom.querySelector(".a-offscreen");
-      const parseLink = 'https://' + linkPage;
-
-      containerProducsCard.innerHTML += `
-      <div class="card-product">
-        <div>
-          ${img.outerHTML}
+        let HTMLBody = await resp.text();
+        const dom = new DOMParser().parseFromString(HTMLBody, "text/html");
+        const img = dom.querySelector(".a-dynamic-image");
+        const title = dom.getElementById("title");
+        const dolarSing = dom.querySelector(".a-price-symbol");
+        const price = dom.querySelector(".a-price-whole");
+        // const savingPercent = dom.querySelector("._cDEzb_apex-savings-percent_nsC2Z");
+        const savingPercent = dom.querySelectorAll('span[class]')
+        const classNames = Array.from(savingPercent).map(span => span.className);
+        const discount = classNames.filter((c) => c.includes('apex-savings-percent'))
+        
+        const realPrice = dom.querySelector(".a-offscreen");
+        const parseLink = 'https://' + linkPage;
+        
+        containerProducsCard.innerHTML += `
+        <div class="card-product">
+            <div>
+              ${img.outerHTML ? img.outerHTML : "not found"}
+            </div>
+    
+            <div>
+              ${title.innerHTML ? title.innerHTML : "not found"}
+            </div>
+            <div>
+              Saving: ${discount.innerHTML ? discount.innerHTML : "not found"}
+            </div>
+            <div>
+              Price: ${dolarSing.innerHTML ? dolarSing.innerHTML : "not found"}${price.innerHTML ? price.innerHTML : "not found"}
+            </div>
+            <div>
+              Recomended price: ${realPrice.innerHTML ? realPrice.innerHTML : "not found"}
+            </div>
+            <a href="${parseLink ? parseLink : "#"}" target="_blank">Product on Amazon</a>
+  
         </div>
-
-        <div>
-          ${title.innerHTML}
-        </div>
-
-        <div>
-          Saving: ${savingPercent.innerHTML}
-        </div>
-        <div>
-          Price: ${dolarSing.innerHTML}${price.innerHTML}
-        </div>
-        <div>
-          Recomended price: ${realPrice.innerHTML}
-        </div>
-        <a href="${parseLink}" target="_blank">Product on Amazon</a>
-
-      </div>
-        `;
-    }
+          `;
+        }catch (err) { 
+          throw new Error(`Element not found : ${err}`);
+        }
+      } 
   }
   return containerProducsCard;
 }
